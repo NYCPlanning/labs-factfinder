16fc745000c9bacd488a571bf5a0e11828c1f73f
Chris Whong <chris@whong.org>
2017-10-26 11:45:23 -0400

    add neighborhood tabulation areas

--- app/sources/index.js
+++ app/sources/index.js
@@ -1,7 +1,9 @@
+import adminBoundaries from './admin-boundaries';
 import censusGeoms from './census-geoms';
 import transportation from './transportation';
 
 export default {
+  adminBoundaries,
   censusGeoms,
   transportation,
 };
