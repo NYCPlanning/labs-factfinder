f736fba957ae1f7dc3e2e61f1313e216d1533ef1
allthesignals <wilburnforce@gmail.com>
2017-10-26 11:37:45 -0400

    Add event to index; new service method

--- app/services/selection.js
+++ app/services/selection.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
+import { computed } from 'ember-decorators/object';
 
 export default Ember.Service.extend({
+  current: null,
+
+  addToSelection(current) {
+    if (current) {
+      // turfjs combination of features
+    }
+  },
 });
