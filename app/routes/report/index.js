028f7938b6b1be19af3d36f35a91abafa403b7e8
Andy Cochran <acochran@planning.nyc.gov>
2017-10-30 14:51:21 -0400

    style active subroutes in report

--- app/routes/report/index.js
+++ app/routes/report/index.js
@@ -3,6 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   beforeModel() {
     // redirect /report to /report/demographic
-    this.replaceWith('user.demographic');
+    this.replaceWith('report.demographic');
   },
 });
