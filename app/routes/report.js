d85a02b5cb08b833ebf8ac6bac014c8802a3e0be
Chris Whong <chris@whong.org>
2017-11-02 10:38:11 -0400

    convert ‘t2’ properties to ‘historic_comparison’

--- app/routes/report.js
+++ app/routes/report.js
@@ -13,8 +13,8 @@ const preserveType = function(array) {
 const aggregateGeos = function(ids, year = 'Y2011-2015') {
   const cleaned = preserveType(ids);
 
-  return `SELECT 
-            SUM(e), 
+  return `SELECT
+            SUM(e),
             SQRT(
               SUM(
                 POWER(m, 2)
@@ -22,36 +22,36 @@ const aggregateGeos = function(ids, year = 'Y2011-2015') {
             ) AS m,
             variable,
             variable || 'E' as variablename
-          FROM 
-            support_fact_finder 
-          WHERE geoid IN (${cleaned}) 
+          FROM
+            support_fact_finder
+          WHERE geoid IN (${cleaned})
             AND year = '${year}'
           GROUP BY variable`;
 };
 
 const generateSelectionSQL = function(...args) {
-  return `SELECT 
-            regexp_replace(lower(variable), '[^A-Za-z0-9]', '_', 'g') as variable, 
+  return `SELECT
+            regexp_replace(lower(variable), '[^A-Za-z0-9]', '_', 'g') as variable,
             regexp_replace(lower(profile), '[^A-Za-z0-9]', '_', 'g') as profile,
             regexp_replace(lower(category), '[^A-Za-z0-9]', '_', 'g') as category,
             type,
-            sum, 
+            sum,
             m
           FROM
             (${aggregateGeos(...args)}) support_fact_finder
-          INNER JOIN support_fact_finder_meta 
+          INNER JOIN support_fact_finder_meta
           ON support_fact_finder_meta.variablename = support_fact_finder.variablename`;
 };
 
 const generateLongitudinalSQL = function(t1, t2) {
   return `
-    SELECT *, 
-      (t2.t2_sum - t1.sum) as delta_sum,
-      (t2.t2_m - t1.m) as delta_m
-    FROM 
+    SELECT *,
+      (t2.historic_comparison_sum - t1.sum) as delta_sum,
+      (t2.historic_comparison_m - t1.m) as delta_m
+    FROM
       (${t1}) t1
-    INNER JOIN 
-      ( SELECT sum AS t2_sum, m AS t2_m, variable as t2_var 
+    INNER JOIN
+      ( SELECT sum AS historic_comparison_sum, m AS historic_comparison_m, variable as t2_var
         FROM (${t2}) t ) t2
     ON t2.t2_var = t1.variable`;
 };
@@ -87,9 +87,9 @@ export default Ember.Route.extend({
     const selectionSQL = generateSelectionSQL(geoids);
     const historicalSQL = generateSelectionSQL(geoids, 'Y2006-2010');
     const longitudinalSQL = generateLongitudinalSQL(selectionSQL, historicalSQL);
-    const geographicComparisonSQL = ` SELECT 
-                              e as comparison_sum, m as comparision_m 
-                            FROM support_fact_finder 
+    const geographicComparisonSQL = ` SELECT
+                              e as comparison_sum, m as comparision_m
+                            FROM support_fact_finder
                             WHERE geoid = '${comparator}'`;
 
     return carto.SQL(longitudinalSQL)
