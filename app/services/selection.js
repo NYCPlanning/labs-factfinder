a01259ca14fbdf2070b6c4b5d40e6e6b124edc1c
Chris Whong <chris@whong.org>
2017-11-03 13:25:11 -0400

    fix eslint error

--- app/services/selection.js
+++ app/services/selection.js
@@ -33,15 +33,7 @@ const NTA_SQL =
     ntacode as geolabel,
     ntacode AS geoid
   FROM support_admin_ntaboundaries`;
-
-  const PUMA_SQL = `
-    SELECT
-        the_geom_webmercator,
-        puma AS geolabel,
-        puma AS geoid
-      FROM nyc_puma
-    `;
-
+  
 const DEFAULT_SELECTION = config.DEFAULT_SELECTION;
 const EMPTY_GEOJSON = { type: 'FeatureCollection', features: [] };
 
