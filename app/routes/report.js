a613c72bd586a46ee185758718a894189d73f08d
allthesignals <wilburnforce@gmail.com>
2017-11-06 13:09:15 -0500

    Better formatting

--- app/routes/report.js
+++ app/routes/report.js
@@ -13,48 +13,48 @@ const generateSelectionSQL = function(geoids, comparator) {
   const ids = preserveType(geoids);
 
   return `
-    WITH filtered_selection AS 
-    ( 
-      SELECT * 
-      FROM   support_fact_finder 
-      WHERE  geoid IN ( ${ids} ) ), base_numbers AS 
-    ( 
-       SELECT * 
-       FROM   ( 
-           SELECT   sum(e)    AS base_sum, 
-                    max(base) AS base_join, 
-                    max(year) AS base_year 
-           FROM     ( 
-                               SELECT     * 
-                               FROM       filtered_selection 
-                               INNER JOIN support_fact_finder_meta_update 
-                               ON         support_fact_finder_meta_update.variablename = filtered_selection.variable ) window_sum
-           WHERE    base = variable 
-           GROUP BY variable, 
-                    "year" ) percentage ) 
-    SELECT *, 
-      (((m / 1.645) / sum) * 100) as cv,
-      (((comparison_m / 1.645) / comparison_sum) * 100) as comparison_cv,
-      regexp_replace(lower(year), '[^A-Za-z0-9]', '_', 'g')     AS year, 
-      regexp_replace(lower(profile), '[^A-Za-z0-9]', '_', 'g')  AS profile, 
-      regexp_replace(lower(category), '[^A-Za-z0-9]', '_', 'g') AS category, 
-      regexp_replace(lower(variable), '[^A-Za-z0-9]', '_', 'g') AS variable 
-    FROM ( 
-         SELECT   sum(e) filter (WHERE  geoid IN ( ${ids} )) AS sum, 
-                  sqrt(sum(power(m, 2)) filter (WHERE geoid IN (${ids}))) AS m,
-                  sum(e) filter (WHERE geoid IN ('${comparator}')) AS comparison_sum,
-                  sqrt( sum( power(m, 2) ) filter (WHERE geoid IN ( '0' ) ) ) AS comparison_m,
-                  year,
-                  variable
-         FROM     support_fact_finder  
-         GROUP BY variable, 
-                  year 
-         ORDER BY variable DESC) aggregated 
-    INNER JOIN      support_fact_finder_meta_update 
-    ON              support_fact_finder_meta_update.variablename = aggregated.variable 
-    LEFT OUTER JOIN base_numbers 
-    ON              base = base_numbers.base_join 
-    AND             year = base_numbers.base_year
+    WITH filtered_selection AS
+      (SELECT *
+       FROM support_fact_finder
+       WHERE geoid IN (${ids}) ),
+         base_numbers AS
+      (SELECT *
+       FROM
+         (SELECT sum(e) AS base_sum,
+                 max(base) AS base_join,
+                 max(YEAR) AS base_year
+          FROM
+            (SELECT *
+             FROM filtered_selection
+             INNER JOIN support_fact_finder_meta_update ON support_fact_finder_meta_update.variablename = filtered_selection.variable) window_sum
+          WHERE base = VARIABLE
+          GROUP BY VARIABLE,
+                   "year") percentage)
+    SELECT *,
+           (((m / 1.645) / SUM) * 100) AS cv,
+           (((comparison_m / 1.645) / comparison_sum) * 100) AS comparison_cv,
+           regexp_replace(lower(YEAR), '[^A-Za-z0-9]', '_', 'g') AS YEAR,
+           regexp_replace(lower(PROFILE), '[^A-Za-z0-9]', '_', 'g') AS PROFILE,
+           regexp_replace(lower(category), '[^A-Za-z0-9]', '_', 'g') AS category,
+           regexp_replace(lower(VARIABLE), '[^A-Za-z0-9]', '_', 'g') AS VARIABLE
+    FROM
+      (SELECT sum(e) filter (
+                             WHERE geoid IN (${ids})) AS SUM,
+              sqrt(sum(power(m, 2)) filter (
+                                            WHERE geoid IN (${ids}))) AS m,
+              sum(e) filter (
+                             WHERE geoid IN ('${comparator}')) AS comparison_sum,
+              sqrt(sum(power(m, 2)) filter (
+                                            WHERE geoid IN ('0') )) AS comparison_m,
+              YEAR,
+              VARIABLE
+       FROM support_fact_finder
+       GROUP BY VARIABLE,
+                YEAR
+       ORDER BY VARIABLE DESC) aggregated
+    INNER JOIN support_fact_finder_meta_update ON support_fact_finder_meta_update.variablename = aggregated.variable
+    LEFT OUTER JOIN base_numbers ON base = base_numbers.base_join
+    AND YEAR = base_numbers.base_year
   `;
 };
 
