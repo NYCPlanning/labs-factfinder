bde940bc69c57e76cd8ce06791202df4b952293c
allthesignals <wilburnforce@gmail.com>
2017-11-08 15:21:18 -0500

    Basic acceptance testing
    This tests
    1. adding geometries
    2. exploding
    3. report generation

--- tests/acceptance/select-geometries-test.js
+++ tests/acceptance/select-geometries-test.js
@@ -1,19 +1,52 @@
 import { test } from 'qunit';
 import startApp from 'labs-nyc-factfinder/tests/helpers/start-app';
 import moduleForAcceptance from 'labs-nyc-factfinder/tests/helpers/module-for-acceptance';
+import config from 'labs-nyc-factfinder/config/environment';
 
 import { click, fillIn, find, findAll, keyEvent, triggerEvent, waitUntil } from 'ember-native-dom-helpers';
 
-let myController;
+const { run } = Ember;
+
+let indexController;
 
 moduleForAcceptance('Acceptance | select geometries', {
   beforeEach() {
     this.application = startApp();
-    myController = this.application.__container__.lookup('controller:index');
+    indexController = this.application.__container__.lookup('controller:index');
   },
 });
 
 test('visiting /', async function(assert) {
   await visit('/');
   assert.equal(currentURL(), '/');
+});
+
+test('visiting /, see no tracks selected', async function(assert) {
+  await visit('/');
+
+  assert.equal($(find('.map-utility-box .stat')).text().trim(), '0');
+});
+
+test('visiting /, adds tracts, counts number of tracts, explodes tracts to blocks', async function(assert) {
+  await visit('/');
+  await run(function() {
+    indexController.get('selection').handleSelectedFeatures(config.SAMPLE_SELECTION.features);
+  });
+
+  assert.equal($(find('.map-utility-box .stat')).text().trim(), '5');
+
+  await click('.explode-block a');
+  await waitUntil(() => ($(find('.map-utility-box .stat')).text().trim()) !== '5', { timeout: 5000 });
+  assert.equal($(find('.map-utility-box .stat')).text().trim(), '113');
+});
+
+test('visiting /, adds tracts, run report', async function(assert) {
+  await visit('/');
+  await run(function() {
+    indexController.get('selection').handleSelectedFeatures(config.SAMPLE_SELECTION.features);
+  });
+
+  await click('.view-report-button');
+  await waitUntil(() => (currentURL() !== '/'));
+  assert.equal(currentURL(), '/report/demographic');
 });
