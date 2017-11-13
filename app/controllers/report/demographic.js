eb3030e7987d7944a74c0d35f78d012e33263833
allthesignals <wilburnforce@gmail.com>
2017-11-13 15:53:56 -0500

    Copy to Clipboard button

--- app/controllers/report/demographic.js
+++ app/controllers/report/demographic.js
@@ -53,4 +53,31 @@ export default Ember.Controller.extend({
       pyramidData,
     };
   },
+
+  actions: {
+    handleCopy() {
+      const el = document.getElementsByTagName('table')[0];
+      const body = document.body;
+      let range;
+      let sel;
+      if (document.createRange && window.getSelection) {
+        range = document.createRange();
+        sel = window.getSelection();
+        sel.removeAllRanges();
+        try {
+          range.selectNodeContents(el);
+          sel.addRange(range);
+        } catch (e) {
+          range.selectNode(el);
+          sel.addRange(range);
+        }
+      } else if (body.createTextRange) {
+        range = body.createTextRange();
+        range.moveToElementText(el);
+        range.select();
+      }
+
+      document.execCommand('copy');
+    },
+  },
 });
