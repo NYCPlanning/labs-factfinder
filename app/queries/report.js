22dc8e36d36994ee4d9fe9093aeec20fca7f1da5
allthesignals <wilburnforce@gmail.com>
2017-11-14 15:29:41 -0500

    merge

--- app/queries/report.js
+++ app/queries/report.js
@@ -9,7 +9,7 @@ const generateReportSQL = function(geoids, comparator) {
     WITH
       filtered_selection AS (
         SELECT *
-        FROM support_fact_finder
+        FROM demographic
         WHERE geoid IN (${ids})
       ),
 
@@ -18,7 +18,7 @@ const generateReportSQL = function(geoids, comparator) {
           sum(e) AS base_sum,
           sqrt(sum(power(m, 2))) AS base_m,
           max(base) AS base_join,
-          max(YEAR) AS base_year
+          max(DATASET) AS base_year
         FROM (
           SELECT *
           FROM filtered_selection
@@ -26,12 +26,12 @@ const generateReportSQL = function(geoids, comparator) {
             ON support_fact_finder_meta_update.variablename = filtered_selection.variable
         ) window_sum
         WHERE base = VARIABLE
-        GROUP BY VARIABLE, "year"
+        GROUP BY VARIABLE, "dataset"
       ),
 
       comparison_selection AS (
         SELECT *
-        FROM support_fact_finder
+        FROM demographic
         WHERE geoid IN ('${comparator}')
       ),
 
@@ -40,7 +40,7 @@ const generateReportSQL = function(geoids, comparator) {
           sum(e) AS comparison_base_sum,
           sqrt(sum(power(m, 2))) AS comparison_base_m,
           max(base) AS comparison_base_join,
-          max(YEAR) AS comparison_base_year
+          max(DATASET) AS comparison_base_year
         FROM (
           SELECT *
           FROM comparison_selection
@@ -48,7 +48,7 @@ const generateReportSQL = function(geoids, comparator) {
             ON support_fact_finder_meta_update.variablename = comparison_selection.variable
         ) window_sum
         WHERE base = VARIABLE
-        GROUP BY VARIABLE, "year"
+        GROUP BY VARIABLE, "dataset"
       )
     SELECT
       *,
@@ -59,7 +59,7 @@ const generateReportSQL = function(geoids, comparator) {
          *,
         (((m / 1.645) / SUM) * 100) AS cv,
         (((comparison_m / 1.645) / comparison_sum) * 100) AS comparison_cv,
-        regexp_replace(lower(YEAR), '[^A-Za-z0-9]', '_', 'g') AS YEAR,
+        regexp_replace(lower(DATASET), '[^A-Za-z0-9]', '_', 'g') AS DATASET,
         regexp_replace(lower(PROFILE), '[^A-Za-z0-9]', '_', 'g') AS PROFILE,
         regexp_replace(lower(category), '[^A-Za-z0-9]', '_', 'g') AS category,
         regexp_replace(lower(VARIABLE), '[^A-Za-z0-9]', '_', 'g') AS VARIABLE,
@@ -73,20 +73,20 @@ const generateReportSQL = function(geoids, comparator) {
           sqrt(sum(power(m, 2)) filter (WHERE geoid IN (${ids}))) AS m,
           sum(e) filter (WHERE geoid IN ('${comparator}')) AS comparison_sum,
           sqrt(sum(power(m, 2)) filter (WHERE geoid IN ('${comparator}') )) AS comparison_m,
-          YEAR,
+          DATASET,
           VARIABLE
-         FROM support_fact_finder
-         GROUP BY VARIABLE, YEAR
+         FROM demographic
+         GROUP BY VARIABLE, DATASET
          ORDER BY VARIABLE DESC
       ) aggregated
       INNER JOIN support_fact_finder_meta_update
         ON support_fact_finder_meta_update.variablename = aggregated.variable
       LEFT OUTER JOIN base_numbers
         ON base = base_numbers.base_join
-        AND YEAR = base_numbers.base_year
+        AND DATASET = base_numbers.base_year
       LEFT OUTER JOIN comparison_base_numbers
         ON base = comparison_base_numbers.comparison_base_join
-        AND YEAR = comparison_base_numbers.comparison_base_year
+        AND DATASET = comparison_base_numbers.comparison_base_year
     ) x
   `;
 };
---
+++ app/utils/nest-report.js
@@ -0,0 +1,11 @@
+import { nest } from 'd3-collection';
+
+export default function nestReport(data) {
+  return nest()
+    .key(d => d.profile)
+    .key(d => d.dataset)
+    .key(d => d.category)
+    .key(d => d.variable)
+    .rollup(d => d[0])
+    .object(data);
+}
