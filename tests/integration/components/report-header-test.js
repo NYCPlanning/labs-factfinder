a89c67bbd117ef3a6cec9fd49bcee82b606b6ca4
Andy Cochran <acochran@planning.nyc.gov>
2017-11-14 12:07:24 -0500

    report header component: vars, stats, map

--- tests/integration/components/report-header-test.js
+++ tests/integration/components/report-header-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('report-map', 'Integration | Component | report map', {
+moduleForComponent('report-header', 'Integration | Component | report header', {
   integration: true
 });
 
@@ -10,15 +10,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{report-map}}`);
+  this.render(hbs`{{report-header}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#report-map}}
+    {{#report-header}}
       template block text
-    {{/report-map}}
+    {{/report-header}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
