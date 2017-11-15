570347b6956e59ae6d49a892815cdddd6829a6f4
Chris Whong <chris@whong.org>
2017-11-15 16:19:46 -0500

    fix eslint errors on table configs

--- app/table-config/housing/value.js
+++ app/table-config/housing/value.js
@@ -1,43 +1,43 @@
 export default [
-{
+  {
     title: 'Owner-occupied units',
     highlight: true,
     data: 'oochu2',
-}, 
-{
+  },
+  {
     title: 'Less than $50,000',
     data: 'vlu50',
-}, 
-{
+  },
+  {
     title: '$50,000 to $99,999',
     data: 'vl50t99',
-}, 
-{
+  },
+  {
     title: '$100,000 to $149,999',
     data: 'vl100t149',
-}, 
-{
+  },
+  {
     title: '$150,000 to $199,999',
     data: 'vl150t199',
-}, 
-{
+  },
+  {
     title: '$200,000 to $299,999',
     data: 'vl200t299',
-}, 
-{
+  },
+  {
     title: '$300,000 to $499,999',
     data: 'vl300t499',
-}, 
-{
+  },
+  {
     title: '$500,000 to $999,999',
     data: 'vl500t999',
-}, 
-{
+  },
+  {
     title: '$1,000,000 or more',
     data: 'vl1milpl',
-}, 
-{
+  },
+  {
     title: 'Median (dollars)',
     data: 'mdvl',
-},
+  },
 ];
--- app/table-config/social/residence-1-year-ago.js
+++ app/table-config/social/residence-1-year-ago.js
@@ -1,53 +1,54 @@
 export default [{
-        title: 'Population 1 year and over',
-        highlight: true,
-        data: 'pop1pl',
-    },
-    {
-        title: 'Same house',
-        data: 'smhs',
-    },
-    {
-        title: 'Lived in a different house',
-        data: 'dfhs1',
-    },
-    {
-        divider: true,
-    },
-    {
-        title: 'Population 1 year and over that lived in a different house',
-        highlight: true,
-        data: 'dfhs2',
-    },
-    {
-        title: 'Different house in the U.S.',
-        data: 'dfhsus',
-    },
-    {
-        indent: 2,
-        title: 'Same county',
-        data: 'dfhssmcnt',
-    },
-    {
-        indent: 2,
-        title: 'Different county',
-        data: 'dfhsdfcnt',
-    },
-    {
-        indent: 4,
-        title: 'Within New York City',
-        data: 'dhdfcntnyc',
-    },
-    {
-        indent: 4,
-        title: 'Outside New York City',
-        data: 'dhnonyc',
-    },
-    {
-        title: 'Abroad',
-        data: 'abroad',
-    } {
-        title: 'Abroad',
-        data: 'abroad',
-    },
-  ];
+  title: 'Population 1 year and over',
+  highlight: true,
+  data: 'pop1pl',
+},
+{
+  title: 'Same house',
+  data: 'smhs',
+},
+{
+  title: 'Lived in a different house',
+  data: 'dfhs1',
+},
+{
+  divider: true,
+},
+{
+  title: 'Population 1 year and over that lived in a different house',
+  highlight: true,
+  data: 'dfhs2',
+},
+{
+  title: 'Different house in the U.S.',
+  data: 'dfhsus',
+},
+{
+  indent: 2,
+  title: 'Same county',
+  data: 'dfhssmcnt',
+},
+{
+  indent: 2,
+  title: 'Different county',
+  data: 'dfhsdfcnt',
+},
+{
+  indent: 4,
+  title: 'Within New York City',
+  data: 'dhdfcntnyc',
+},
+{
+  indent: 4,
+  title: 'Outside New York City',
+  data: 'dhnonyc',
+},
+{
+  title: 'Abroad',
+  data: 'abroad',
+},
+{
+  title: 'Abroad',
+  data: 'abroad',
+},
+];
