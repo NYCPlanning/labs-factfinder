6f9fac033fa0c2e630678f79bf0150e1e362fccb
Chris Whong <chris@whong.org>
2017-11-14 23:30:03 -0500

    add employment and commute configs

---
+++ app/table-config/economic/employment-status.js
@@ -0,0 +1,87 @@
+export default [
+  {
+    title: 'Population 16 years and over',
+    highlight: true,
+    data: 'pop16pl',
+  },
+  {
+    title: 'In labor force',
+    data: 'lf',
+  },
+  {
+    title: 'Civilian labor force',
+    data: 'cvlf1',
+  },
+  {
+    title: 'Employed',
+    data: 'cvem16pl1',
+  },
+  {
+    title: 'Unemployed',
+    data: 'cvlfuem1',
+  },
+  {
+    title: 'Armed Forces',
+    data: 'lfarmdf',
+  },
+  {
+    title: 'Not in labor force',
+    data: 'nlf1',
+  },
+  {
+    divider: true,
+  },
+  {
+    title: 'Civilian labor force',
+    highlight: true,
+    data: 'cvlf2',
+  },
+  {
+    title: 'Unemployed',
+    data: 'cvlfuem2',
+  },
+  {
+    divider: true,
+  },
+  {
+    title: 'Females 16 years and over',
+    highlight: true,
+    data: 'f16pl',
+  },
+  {
+    title: 'In labor force',
+    data: 'f16pllf',
+  },
+  {
+    title: 'Civilian labor force',
+    data: 'f16plcvlf',
+  },
+  {
+    title: 'Employed',
+    data: 'f16plclfe',
+  },
+  {
+    divider: true,
+  },
+  {
+    title: 'Own children under 6 years',
+    highlight: true,
+    data: 'ochu6',
+  },
+  {
+    title: 'All parents in family in labor force',
+    data: 'ochu6plf',
+  },
+  {
+    divider: true,
+  },
+  {
+    title: 'Own children 6 to 17 years',
+    highlight: true,
+    data: 'och6t17',
+  },
+  {
+    title: 'All parents in family in labor force',
+    data: 'oc6t17plf',
+  },
+];
