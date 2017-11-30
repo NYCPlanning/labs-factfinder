349a67fe81b8811bb8f597dde6d65771dc10ab0d
Chengying <ceciliaheny@gmail.com>
2017-11-30 12:13:30 -0500

    fixed format

--- app/chart-config/demographic/asian-subgroup.js
+++ app/chart-config/demographic/asian-subgroup.js
@@ -1,15 +1,14 @@
 export default [
-{
+  {
     property: 'asnseast',
     label: 'Southeast Asian',
-},
-{
+  },
+  {
     property: 'asnsouth',
     label: 'South Asian',
-},
-{
+  },
+  {
     property: 'asnchinot',
     label: 'Chinese, except Taiwanese',
-},
+  },
 ];
-
--- app/chart-config/demographic/hispanic-subgroup.js
+++ app/chart-config/demographic/hispanic-subgroup.js
@@ -1,22 +1,22 @@
 export default [
-{
+  {
     property: 'hsppr',
     label: 'Puerto Rican',
-},
-{
+  },
+  {
     property: 'hspdom',
     label: 'Dominican',
-},
-{
+  },
+  {
     property: 'hspme',
     label: 'Mexican',
-},
-{
+  },
+  {
     property: 'hspcam',
     label: 'Central American',
-},
-{
+  },
+  {
     property: 'hspsam',
     label: 'South American',
-},
+  },
 ];
