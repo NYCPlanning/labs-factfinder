24f47e224376de5ee547ac1bb033cd957cb27d23
Chengying <ceciliaheny@gmail.com>
2017-11-30 16:15:27 -0500

    fixed test error

--- app/components/acs-bar.js
+++ app/components/acs-bar.js
@@ -7,6 +7,7 @@ import { axisBottom } from 'd3-axis';
 import { format } from 'd3-format';
 import numeral from 'numeral';// eslint-disable-line
 import mungeBarChartData from '../utils/munge-bar-chart-data';
+import { transition } from 'd3-transition'; // eslint-disable-line
 
 const translation = (x, y) => `translate(${x},${y})`;
 
@@ -29,19 +30,19 @@ const HorizontalBar = Ember.Component.extend(ResizeAware, {
   resizeHeightSensitive: true,
   loading: false,
 
-  getBarChartData(d, config) {
-    return config.map(({ property, label }) =>
-      ({
-        percent: d[property].percent,
-        sum: d[property].sum,
-        moe: d[property].m,
-        percent_m: d[property].percent_m,
-        comparison_percent: d[property].comparison_percent,
-        comparison_percent_m: d[property].comparison_percent_m,
-        group: label,
-        classValue: property,
-      }));
-  },
+  // getBarChartData(d, config) {
+  //   return config.map(({ property, label }) =>
+  //     ({
+  //       percent: d[property].percent,
+  //       sum: d[property].sum,
+  //       moe: d[property].m,
+  //       percent_m: d[property].percent_m,
+  //       comparison_percent: d[property].comparison_percent,
+  //       comparison_percent_m: d[property].comparison_percent_m,
+  //       group: label,
+  //       classValue: property,
+  //     }));
+  // },
 
   didRender() {
     this.createChart();
@@ -116,178 +117,177 @@ const HorizontalBar = Ember.Component.extend(ResizeAware, {
       .attr('width', width + margin.left + margin.right)
       .attr('height', height + margin.top + margin.bottom);
 
-    Promise.resolve(data).then((raw) => {
-      const rawData = mungeBarChartData(config, raw);
-      const y = scaleBand()
-        .domain(rawData.map(d => d.group))
-        .range([0, height])
-        .paddingOuter(0)
-        .paddingInner(0.1);
 
-      const x = scaleLinear()
-        .domain([0, this.get('xMax') ? this.get('xMax') : max(rawData, d => d.percent)])
-        .range([textWidth, width]);
+    const rawData = mungeBarChartData(config, data);
+    const y = scaleBand()
+      .domain(rawData.map(d => d.group))
+      .range([0, height])
+      .paddingOuter(0)
+      .paddingInner(0.1);
+
+    const x = scaleLinear()
+      .domain([0, this.get('xMax') ? this.get('xMax') : max(rawData, d => d.percent)])
+      .range([textWidth, width]);
 
       // add bar text
-      const groupLabels = svg.selectAll('.typelabel')
-        .data(rawData, d => d.group);
+    const groupLabels = svg.selectAll('.typelabel')
+      .data(rawData, d => d.group);
 
-      groupLabels.enter().append('text')
-        .attr('class', 'label typelabel')
-        .attr('text-anchor', 'end')
-        .attr('alignment-baseline', 'top')
-        .attr('x', (width / 2) - 10)
-        .attr('width', textWidth);
+    groupLabels.enter().append('text')
+      .attr('class', 'label typelabel')
+      .attr('text-anchor', 'end')
+      .attr('alignment-baseline', 'top')
+      .attr('x', (width / 2) - 10)
+      .attr('width', textWidth);
 
-      groupLabels.transition().duration(300)
-        .attr('y', d => y(d.group) + y.bandwidth() + -14)
-        .text(d => `${d.group}`);
+    groupLabels.transition().duration(300)
+      .attr('y', d => y(d.group) + y.bandwidth() + -14)
+      .text(d => `${d.group}`);
 
-      groupLabels.exit().remove();
+    groupLabels.exit().remove();
 
-      // DRAW AXES
-      const bottomAxis = axisBottom()
-        .scale(x)
-        .ticks(5)
-        .tickFormat(format('.0%'));
-
-
-      svg.select('.axis-bottom')
-        .attr('transform', translation(0, height))
-
-        .call(bottomAxis);
-
-      // update positioning and text of top-labels
+    // DRAW AXES
+    const bottomAxis = axisBottom()
+      .scale(x)
+      .ticks(5)
+      .tickFormat(format('.0%'));
 
 
-      // draw bars
+    svg.select('.axis-bottom')
+      .attr('transform', translation(0, height))
 
-      const bars = svg.selectAll('.buildingsbar')
-        .data(rawData, d => d.group);
+      .call(bottomAxis);
 
-      bars.enter()
-        .append('rect')
-        .attr('class', d => `buildingsbar ${d.classValue}`)
-        .attr('fill', (d) => {
-          if (d.color) return d.color;
-          return '#60acbf';
-        })
-        .attr('x', x(0))
-        .attr('width', d => x(d.percent) - textWidth)
-        .attr('y', d => y(d.group))
-        .attr('height', y.bandwidth() - 14)
+    // update positioning and text of top-labels
 
 
-        .attr('rx', 2)
-        .attr('ry', 2)
-        .on('mouseover', (d) => {
-          handleMouseOver(d);
-        })
-        .on('mouseout', handleMouseOut);
+    // draw bars
+
+    const bars = svg.selectAll('.buildingsbar')
+      .data(rawData, d => d.group);
+
+    bars.enter()
+      .append('rect')
+      .attr('class', d => `buildingsbar ${d.classValue}`)
+      .attr('fill', (d) => {
+        if (d.color) return d.color;
+        return '#60acbf';
+      })
+      .attr('x', x(0))
+      .attr('width', d => x(d.percent) - textWidth)
+      .attr('y', d => y(d.group))
+      .attr('height', y.bandwidth() - 14)
 
 
-      bars.transition().duration(300)
-        .attr('x', x(0))
-        .attr('width', d => x(d.percent) - textWidth);
-
-      bars.exit().remove();
-
-      // draw MOE
-
-      const moebars = svg.selectAll('.moebar')
-        .data(rawData, d => d.group);
+      .attr('rx', 2)
+      .attr('ry', 2)
+      .on('mouseover', (d) => {
+        handleMouseOver(d);
+      })
+      .on('mouseout', handleMouseOut);
 
 
-      const xFunctionMOE = (d) => {
-        if (d.percent_m > d.percent) return x(0);
-        return x(d.percent) - x(d.percent_m) - -textWidth;
-      };
+    bars.transition().duration(300)
+      .attr('x', x(0))
+      .attr('width', d => x(d.percent) - textWidth);
 
-      const widthFunctionMOE = (d) => {
-        const defaultWidth = (x(d.percent_m) - textWidth) * 2;
-        if (d.percent_m > d.percent) {
-          const newWidth = defaultWidth - (x(d.percent_m - d.percent) - textWidth);
-          return newWidth;
-        }
-        return defaultWidth;
-      };
+    bars.exit().remove();
+
+    // draw MOE
+
+    const moebars = svg.selectAll('.moebar')
+      .data(rawData, d => d.group);
 
 
-      moebars.enter()
-        .append('rect')
-        .attr('class', d => `moebar ${d.classValue}`)
-        .attr('fill', (d) => {
-          if (d.color) return d.color;
-          return '#2e6472';
-        })
-        .attr('opacity', 0.4)
-        .attr('x', xFunctionMOE)
-        .attr('y', d => y(d.group) + (y.bandwidth() / 2) + -10)
-        .attr('height', 6)
-        .attr('width', widthFunctionMOE);
+    const xFunctionMOE = (d) => {
+      if (d.percent_m > d.percent) return x(0);
+      return x(d.percent) - x(d.percent_m) - -textWidth;
+    };
 
-      moebars.transition().duration(300)
-        .attr('x', xFunctionMOE)
-        .attr('width', widthFunctionMOE);
-
-      moebars.exit().remove();
+    const widthFunctionMOE = (d) => {
+      const defaultWidth = (x(d.percent_m) - textWidth) * 2;
+      if (d.percent_m > d.percent) {
+        const newWidth = defaultWidth - (x(d.percent_m - d.percent) - textWidth);
+        return newWidth;
+      }
+      return defaultWidth;
+    };
 
 
-      // draw Comparison MOE
+    moebars.enter()
+      .append('rect')
+      .attr('class', d => `moebar ${d.classValue}`)
+      .attr('fill', (d) => {
+        if (d.color) return d.color;
+        return '#2e6472';
+      })
+      .attr('opacity', 0.4)
+      .attr('x', xFunctionMOE)
+      .attr('y', d => y(d.group) + (y.bandwidth() / 2) + -10)
+      .attr('height', 6)
+      .attr('width', widthFunctionMOE);
 
-      const comparisonMOEbars = svg.selectAll('.comparisonMoebar')
-        .data(rawData, d => d.group);
+    moebars.transition().duration(300)
+      .attr('x', xFunctionMOE)
+      .attr('width', widthFunctionMOE);
+
+    moebars.exit().remove();
 
 
-      const xFunctionComparisonMOE = d => x(d.comparison_percent)
+    // draw Comparison MOE
+
+    const comparisonMOEbars = svg.selectAll('.comparisonMoebar')
+      .data(rawData, d => d.group);
+
+
+    const xFunctionComparisonMOE = d => x(d.comparison_percent)
       - x(d.comparison_percent_m) -
       -textWidth;
 
-      const widthFunctionComparisonMOE = d => (x(d.comparison_percent_m) - textWidth) * 2;
-      comparisonMOEbars.enter()
-        .append('rect')
-        .attr('class', d => `comparisonMoebar ${d.classValue}`)
-        .attr('fill', (d) => {
-          if (d.color) return d.color;
-          return '#000000';
-        })
-        .attr('opacity', 1)
-        .attr('x', xFunctionComparisonMOE)
-        .attr('y', d => y(d.group) + (y.bandwidth() / 2) + -7)
-        .attr('height', 1)
-        .attr('width', widthFunctionComparisonMOE);
+    const widthFunctionComparisonMOE = d => (x(d.comparison_percent_m) - textWidth) * 2;
+    comparisonMOEbars.enter()
+      .append('rect')
+      .attr('class', d => `comparisonMoebar ${d.classValue}`)
+      .attr('fill', (d) => {
+        if (d.color) return d.color;
+        return '#000000';
+      })
+      .attr('opacity', 1)
+      .attr('x', xFunctionComparisonMOE)
+      .attr('y', d => y(d.group) + (y.bandwidth() / 2) + -7)
+      .attr('height', 1)
+      .attr('width', widthFunctionComparisonMOE);
 
 
-      comparisonMOEbars.transition().duration(300)
-        .attr('x', xFunctionComparisonMOE)
-        .attr('width', widthFunctionComparisonMOE);
+    comparisonMOEbars.transition().duration(300)
+      .attr('x', xFunctionComparisonMOE)
+      .attr('width', widthFunctionComparisonMOE);
 
 
-      comparisonMOEbars.exit().remove();
+    comparisonMOEbars.exit().remove();
 
 
-      // draw comparison dots
+    // draw comparison dots
 
 
-      const comparisonBars = svg.selectAll('.comparisonbar')
-        .data(rawData, d => d.group);
+    const comparisonBars = svg.selectAll('.comparisonbar')
+      .data(rawData, d => d.group);
 
 
-      const cxFunction = d => x(d.comparison_percent);
-      comparisonBars.enter()
-        .append('circle')
-        .attr('fill', '#FFF')
-        .attr('stroke', '#000')
-        .attr('class', d => `comparisonbar ${d.classValue}`)
-        .attr('cx', cxFunction)
-        .attr('cy', d => y(d.group) + (y.bandwidth() / 2) + -6.5)// yScale.step()
-        .attr('r', 2.5);
+    const cxFunction = d => x(d.comparison_percent);
+    comparisonBars.enter()
+      .append('circle')
+      .attr('fill', '#FFF')
+      .attr('stroke', '#000')
+      .attr('class', d => `comparisonbar ${d.classValue}`)
+      .attr('cx', cxFunction)
+      .attr('cy', d => y(d.group) + (y.bandwidth() / 2) + -6.5)// yScale.step()
+      .attr('r', 2.5);
 
-      comparisonBars.transition().duration(300)
-        .attr('cx', cxFunction);
+    comparisonBars.transition().duration(300)
+      .attr('cx', cxFunction);
 
-      comparisonBars.exit().remove();
-    });
+    comparisonBars.exit().remove();
   },
 });
 
