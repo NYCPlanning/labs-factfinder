070c07c0cefbd544ef323bc79d5319bf54045e76
allthesignals <wilburnforce@gmail.com>
2017-11-30 21:52:26 -0500

    Complete profile

--- app/controllers/profile/census.js
+++ app/controllers/profile/census.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import popDensity from '../../table-config/decennial/pop-density';
 import sexAge from '../../table-config/decennial/sex-age';
 import mutuallyExclRaceHisp from '../../table-config/decennial/mutually-excl-race-hisp';
 import hispSubgroup from '../../table-config/decennial/hisp-subgroup';
@@ -6,8 +7,12 @@ import asianSubgroup from '../../table-config/decennial/asian-subgroup';
 import headHousehold from '../../table-config/decennial/head-household';
 import householdType from '../../table-config/decennial/household-type';
 import housingOccupancy from '../../table-config/decennial/housing-occupancy';
+import housingTenure from '../../table-config/decennial/housing-tenure';
+import tenureAgeHouseholder from '../../table-config/decennial/tenure-age-householder';
+import householdSize from '../../table-config/decennial/household-size';
 
 export default Ember.Controller.extend({
+  popDensity,
   sexAge,
   mutuallyExclRaceHisp,
   hispSubgroup,
@@ -15,4 +20,7 @@ export default Ember.Controller.extend({
   headHousehold,
   householdType,
   housingOccupancy,
+  housingTenure,
+  tenureAgeHouseholder,
+  householdSize,
 });
---
+++ app/table-config/decennial/housing-tenure.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    title: 'Occupied housing units',
+    data: 'ochu_2',
+  },
+  {
+    title: 'Owner-occupied housing units',
+    data: 'oochu',
+  },
+  {
+    title: 'Renter-occupied housing units',
+    data: 'rochu_1',
+  },
+  {
+    title: 'Average household size of owner-occupied units',
+    data: 'avhhszooc',
+  },
+  {
+    title: 'Average household size of renter-occupied units',
+    data: 'avhhszroc',
+  },
+];
