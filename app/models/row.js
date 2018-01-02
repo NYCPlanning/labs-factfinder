2a6f8c40af9a63f0a84452f7b5df3894599ddf7b
allthesignals <wilburnforce@gmail.com>
2018-01-02 15:51:29 -0500

    New computeds for row, moved out of components

--- app/models/row.js
+++ app/models/row.js
@@ -1,4 +1,59 @@
 import DS from 'ember-data';
+import { computed } from 'ember-decorators/object';
+
+// for number >=0
+export function decimalFormat(number, decimal) {
+  if ((number > 0) || (number === 0)) {
+    let x;
+    if (decimal === 1) {
+      x = number.toFixed(1);
+    } else if (decimal === 2) {
+      x = number.toFixed(2);
+    } else {
+      x = number.toFixed(0);
+    }
+
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  }
+
+  return '';
+}
+
+// for all numbers
+export function decimalFormatAll(number, decimal) {
+  let x;
+  if (isNaN(number)) {
+    return '';
+  }
+
+  if (decimal === 1) {
+    x = number.toFixed(1);
+  } else if (decimal === 2) {
+    x = number.toFixed(2);
+  } else {
+    x = number.toFixed(0);
+  }
+
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+export function decimalOnePlace(number) {
+  const floated = parseFloat(number);
+  if (isNaN(floated)) {
+    return '';
+  }
+
+  return floated.toFixed(1);
+}
+
+export function decimalOnePlacePercent(number) {
+  const floated = parseFloat(number);
+  if (isNaN(floated)) {
+    return '';
+  }
+
+  return `${(floated * 100).toFixed(1)}%`;
+}
 
 export default DS.Model.extend({
   base: DS.attr('number'),
@@ -18,7 +73,7 @@ export default DS.Model.extend({
   comparison_percent_m: DS.attr('number'),
   comparison_sum: DS.attr('number'),
   cv: DS.attr('number'),
-  dataset: DS.attr('string'),
+  dataset: DS.attr('string'), // year
   infltnfctr10to16: DS.attr('number'),
   m: DS.attr('number'),
   notinprofile: DS.attr('boolean'),
@@ -33,5 +88,86 @@ export default DS.Model.extend({
   unittype: DS.attr('string'),
   variable: DS.attr('string'),
   variablename: DS.attr('string'),
-  _order: DS.attr('number'),
+
+  @computed('sum', 'cv')
+  selectedCV(sum, cv) {
+    const floatedSum = parseFloat(sum);
+    const floatedCv = parseFloat(cv);
+    if (floatedSum > 0) {
+      return decimalOnePlace(floatedCv);
+    }
+
+    return '';
+  },
+
+  @computed('sum', 'percent')
+  selectedPercent(sum, percent) {
+    const floatedSum = parseFloat(sum);
+    const floatedPercent = parseFloat(percent);
+
+    if (floatedSum > 0) {
+      return decimalOnePlacePercent(floatedPercent);
+    }
+
+    return '';
+  },
+
+  @computed('sum', 'percent_m')
+  selectedPercentM(sum, percent_m) {
+    const floatedSum = parseFloat(sum);
+    const floatedZ = parseFloat(percent_m);
+    if (floatedSum > 0) {
+      return decimalOnePlacePercent(floatedZ);
+    }
+
+    return '';
+  },
+
+  @computed('comparison_sum', 'comparison_cv')
+  comparisonCV(sum, cv) {
+    const floatedSum = parseFloat(sum);
+    const floatedCv = parseFloat(cv);
+
+    if (floatedSum > 0) {
+      return decimalOnePlace(floatedCv);
+    }
+
+    return '';
+  },
+
+  @computed('comparison_sum', 'comparison_percent')
+  comparisonPercent(sum, percent) {
+    const floatedSum = parseFloat(sum);
+    const floatedPercent = parseFloat(percent);
+
+    if (floatedSum > 0) {
+      return decimalOnePlacePercent(floatedPercent);
+    }
+
+    return '';
+  },
+
+  @computed('comparison_sum', 'comparison_percent_m')
+  comparisonPercentM(sum, percentM) {
+    const floatedSum = parseFloat(sum);
+    const floatedZ = parseFloat(percentM);
+    if (floatedSum > 0) {
+      return decimalOnePlacePercent(floatedZ);
+    }
+
+    return '';
+  },
+
+  @computed('percent', 'comparison_percent')
+  differencePercent(percent, comparisonPercent) {
+    const floatedPercent = parseFloat(percent);
+    const floatedComparison = parseFloat(comparisonPercent);
+    const difference = (floatedPercent - floatedComparison) * 100;
+
+    if (isNaN(floatedPercent) || isNaN(floatedComparison)) {
+      return '';
+    }
+
+    return difference.toFixed(1);
+  },
 });
