c1f878040b4437e44703f43693622c57ad4b3453
allthesignals <wilburnforce@gmail.com>
2018-01-04 15:13:09 -0500

    Cleanup, rename decennial profile

--- app/controllers/profile/census.js
+++ app/controllers/profile/census.js
@@ -1,17 +1,20 @@
 import Ember from 'ember';
 import { computed } from 'ember-decorators/object'; // eslint-disable-line
+import decennial from '../../table-config/decennial';
 
-import popDensity from '../../table-config/decennial/pop-density';
-import sexAge from '../../table-config/decennial/sex-age';
-import mutuallyExclRaceHisp from '../../table-config/decennial/mutually-excl-race-hisp';
-import hispSubgroup from '../../table-config/decennial/hisp-subgroup';
-import asianSubgroup from '../../table-config/decennial/asian-subgroup';
-import headHousehold from '../../table-config/decennial/head-household';
-import householdType from '../../table-config/decennial/household-type';
-import housingOccupancy from '../../table-config/decennial/housing-occupancy';
-import housingTenure from '../../table-config/decennial/housing-tenure';
-import tenureAgeHouseholder from '../../table-config/decennial/tenure-age-householder';
-import householdSize from '../../table-config/decennial/household-size';
+const {
+  populationDensity,
+  sexAndAge,
+  mutuallyExclusiveRaceHispanicOrigin,
+  hispanicSubgroup,
+  asianSubgroup,
+  relationshipToHeadOfHouseholdHouseholder,
+  householdType,
+  housingOccupancy,
+  housingTenure,
+  tenureByAgeOfHouseholder,
+  householdSize,
+} = decennial;
 
 const { inject: { controller } } = Ember;
 
@@ -20,14 +23,14 @@ export default Ember.Controller.extend({
   @computed('model.taskInstance.value') data(value) { return value; },
 
   asianSubgroup,
-  headHousehold,
-  hispSubgroup,
+  relationshipToHeadOfHouseholdHouseholder,
+  hispanicSubgroup,
   householdSize,
   householdType,
   housingOccupancy,
   housingTenure,
-  mutuallyExclRaceHisp,
-  popDensity,
-  sexAge,
-  tenureAgeHouseholder,
+  mutuallyExclusiveRaceHispanicOrigin,
+  populationDensity,
+  sexAndAge,
+  tenureByAgeOfHouseholder,
 });
--- app/table-config/decennial/index.js
+++ app/table-config/decennial/index.js
@@ -1,28 +1,27 @@
 // @create-index
 
 import asianSubgroup from './asian-subgroup';
-import headHousehold from './head-household';
-import hispSubgroup from './hisp-subgroup';
+import relationshipToHeadOfHouseholdHouseholder from './relationship-to-head-of-household--householder';
+import hispanicSubgroup from './hispanic-subgroup';
 import householdSize from './household-size';
 import householdType from './household-type';
 import housingOccupancy from './housing-occupancy';
 import housingTenure from './housing-tenure';
-import mutuallyExclRaceHisp from './mutually-excl-race-hisp';
-import popDensity from './pop-density';
-import sexAge from './sex-age';
-import tenureAgeHouseholder from './tenure-age-householder';
+import mutuallyExclusiveRaceHispanicOrigin from './mutually-exclusive-race---hispanic-origin';
+import populationDensity from './population-density';
+import sexAndAge from './sex-and-age';
+import tenureByAgeOfHouseholder from './tenure-by-age-of-householder';
 
 export default {
   asianSubgroup,
-  headHousehold,
-  hispSubgroup,
+  relationshipToHeadOfHouseholdHouseholder,
+  hispanicSubgroup,
   householdSize,
   householdType,
   housingOccupancy,
   housingTenure,
-  mutuallyExclRaceHisp,
-  popDensity,
-  sexAge,
-  tenureAgeHouseholder,
+  mutuallyExclusiveRaceHispanicOrigin,
+  populationDensity,
+  sexAndAge,
+  tenureByAgeOfHouseholder,
 };
-
