2cf15da774d2f9eeecadb6deca6aa08e46dd2074
Chris Whong <chris@whong.org>
2018-02-05 23:06:53 -0500

    consume new decennial census api

--- app/adapters/row.js
+++ app/adapters/row.js
@@ -2,18 +2,24 @@ import DS from 'ember-data';
 import carto from '../utils/carto';
 import generateProfileSQL from '../queries/profile';
 import decennialProfile from '../queries/decennial-profile';
+import Environment from '../config/environment';
+
+const { SupportServiceHost } = Environment;
 
 export default DS.JSONAPIAdapter.extend({
   query(store, modelType, query) {
-    const { geoids, comparator, type, category } = query;
-    let selectionSQL;
-    if (type === 'decennial') {
-      selectionSQL = decennialProfile(geoids, category, comparator);
-    } else {
-      selectionSQL = generateProfileSQL(geoids, comparator, type);
-    }
+    const { selectionId, comparator, type } = query;
+    // let selectionSQL;
+    // if (type === 'decennial') {
+    //   selectionSQL = decennialProfile(geoids, category, comparator);
+    // } else {
+    //   selectionSQL = generateProfileSQL(geoids, comparator, type);
+    // }
 
-    return carto.SQL(selectionSQL, 'json', 'post');
+    const URL = `${SupportServiceHost}/profile/${selectionId}/decennial`;
+
+    return fetch(URL)
+      .then(d => d.json());
   },
 
   keyForAttribute(key) {
--- app/routes/profile/census.js
+++ app/routes/profile/census.js
@@ -15,19 +15,7 @@ export default Ember.Route.extend({
   },
 
   model(params, { queryParams: { comparator = '0' } }) {
-    return {
-      sex_age: this.get('fetchCategory').perform('decennial_sex_age', comparator),
-      population_density: this.get('fetchCategory').perform('decennial_population_density', comparator),
-      mutually_exclusive_race: this.get('fetchCategory').perform('decennial_mutually_exclusive_race', comparator),
-      hispanic_subgroup: this.get('fetchCategory').perform('decennial_hispanic_subgroup', comparator),
-      asian_subgroup: this.get('fetchCategory').perform('decennial_asian_subgroup', comparator),
-      relationship_head_householder: this.get('fetchCategory').perform('decennial_relationship_head_householder', comparator),
-      household_type: this.get('fetchCategory').perform('decennial_household_type', comparator),
-      housing_occupancy: this.get('fetchCategory').perform('decennial_housing_occupancy', comparator),
-      housing_tenure: this.get('fetchCategory').perform('decennial_housing_tenure', comparator),
-      tenure_by_age: this.get('fetchCategory').perform('decennial_tenure_by_age', comparator),
-      household_size: this.get('fetchCategory').perform('decennial_household_size', comparator),
-    };
+    return this.get('fetchDecennialData').perform(comparator)
   },
 
   afterModel(model, transition) {
@@ -36,10 +24,10 @@ export default Ember.Route.extend({
     this.controllerFor('profile').set('tab', targetName);
   },
 
-  fetchCategory: task(function* (category, comparator) {
-    const geoids = this.get('selection.current.features').mapBy('properties.geoid');
+  fetchDecennialData: task(function* (comparator) {
+    const selectionId = this.modelFor('profile').id;
     const profileData = yield this.get('store')
-      .query('row', { geoids, type: 'decennial', category, comparator })
+      .query('row', { selectionId, type: 'decennial', comparator })
       .then(rows => rows.toArray());
 
     const nestedModel = nestProfile(profileData, 'year', 'variable');
