ce5c571bd832f675f5b14d38772d53912b273701
Chris Whong <chris@whong.org>
2018-02-22 08:52:31 -0500

    hotfix - temporarily remove choropleths

--- app/sources/index.js
+++ app/sources/index.js
@@ -1,11 +1,11 @@
 import adminBoundaries from './admin-boundaries';
 import censusGeoms from './census-geoms';
-import choropleths from './choropleths';
+// import choropleths from './choropleths';
 import transportation from './transportation';
 
 export default {
   adminBoundaries,
   censusGeoms,
-  choropleths,
+  // choropleths,
   transportation,
 };
