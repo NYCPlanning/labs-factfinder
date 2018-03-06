eead2398426b28906a78934b630b9600bbd99994
allthesignals <wilburnforce@gmail.com>
2018-03-06 16:59:52 -0500

    Update env for screenshotting

--- config/environment.js
+++ config/environment.js
@@ -69,7 +69,8 @@ module.exports = function(environment) {
     ENV.SupportServiceHost = 'http://localhost:4000';
   }
 
-  if (environment === 'development') {
+  // generates a screenshot for diffing
+  if (environment === 'development' || environment === 'devlocal') {
     (async () => {
       const browser = await puppeteer.launch();
       const page = await browser.newPage();
@@ -79,11 +80,6 @@ module.exports = function(environment) {
 
       await browser.close();
     })();
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
 
   if (environment === 'test') {
