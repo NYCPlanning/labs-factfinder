fc6032b08c074e4248d81602b2bc0aef343c2a19
allthesignals <wilburnforce@gmail.com>
2018-03-06 12:08:15 -0500

    Include screenshotter at bottom

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -27,3 +27,16 @@ module.exports = function(defaults) {
 
   return app.toTree();
 };
+
+// grab a screenshot
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('http://localhost:4200/profile/1/demographic?charts=false&reliability=true');
+  await page.waitForSelector('h3#sex-and-age');
+  await page.screenshot({ path: 'latest.png', fullPage: true });
+
+  await browser.close();
+})();
