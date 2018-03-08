22e913ab0a216428f75e0dae776f82cd410743e6
allthesignals <wilburnforce@gmail.com>
2018-03-08 16:49:35 -0500

    Fix ESLint

--- app/services/scroller.js
+++ app/services/scroller.js
@@ -4,5 +4,5 @@ import Scroller from 'ember-scroll-to/services/scroller';
 export default Scroller.extend({
   scrollable: Ember.computed(function() {
     return Ember.$('#profile-content');
-  })
+  }),
 });
