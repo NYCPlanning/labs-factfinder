6ef46a597ae6285a6a5023dd85c96ecc82446869
Andy Cochran <acochran@planning.nyc.gov>
2018-12-03 11:58:43 -0500

    nix index->about redirect

--- app/routes/index.js
+++ app/routes/index.js
@@ -7,13 +7,6 @@ import { hash } from 'rsvp';
 export default class IndexRoute extends Route {
   @service('layerGroups') layerGroupService
 
-  beforeModel = (transition) => {
-    // only transition to about if index is loaded and there is no hash
-    if (transition.intent.url === '/' && window.location.href.split('#').length < 2) {
-      this.transitionTo('about');
-    }
-  }
-
   async model() {
     // const filter = ['any', ['==', true, true]];
 
