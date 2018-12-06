59b3da7608aee52b226028dd47de06cf08584503
trbmcginnis <taylormcgin@gmail.com>
2018-12-06 13:33:40 -0500

    remove old layer-groups

--- app/layer-groups/index.js
+++ app/layer-groups/index.js
@@ -1,25 +1,14 @@
-// import boroughs from './boroughs';
 import censusBlocks from './census-blocks';
 import censusTracts from './census-tracts';
 import choropleths from './choropleths';
-// import communityDistricts from './communitydistricts';
-// import neighborhoodTabulationAreas from './neighborhood-tabulation-areas';
 import neighborhoodTabulationAreasSelection from './neighborhood-tabulation-areas-selection';
-// import nyccouncildistricts from './nyccouncildistricts';
-// import nycPumas from './nyc-pumas';
 import nycPumasSelection from './nyc-pumas-selection';
-// import subway from './subway';
+
 
 export default {
-  // boroughs,
   censusBlocks,
   censusTracts,
   choropleths,
-  // communityDistricts,
-  // neighborhoodTabulationAreas,
   neighborhoodTabulationAreasSelection,
-  // nyccouncildistricts,
-  // nycPumas,
   nycPumasSelection,
-  // subway,
 };
