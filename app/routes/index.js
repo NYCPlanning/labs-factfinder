10b32487439d7a0a32bd18fa964787e99573fd7a
allthesignals <wilburnforce@gmail.com>
2018-12-13 15:38:13 -0500

    Make selection layers clickable for puma, nta

--- app/routes/index.js
+++ app/routes/index.js
@@ -7,17 +7,17 @@ export default Route.extend({
   layerGroupService: service('layerGroups'),
 
   async model() {
-    // const filter = ['any', ['==', true, true]];
-
     const layerGroups = await this.store.query('layer-group', {
       'layer-groups': [
-        { id: 'neighborhood-tabulation-areas', visible: false },
         { id: 'subway', visible: false },
         { id: 'nyc-council-districts', visible: false },
         { id: 'community-districts', visible: false },
         { id: 'boroughs', visible: false },
         { id: 'bk-qn-mh-boundary', visible: true, layers: [{ tooltipable: false }] },
-        { id: 'nyc-pumas', visible: false },
+
+        // Census selection groups
+        { id: 'nyc-pumas', visible: false, layers: [{}, {}, {}, { clickable: true }] },
+        { id: 'neighborhood-tabulation-areas', visible: false, layers: [{}, {}, {}, { clickable: true }] },
         { id: 'census-tracts', visible: true, layers: [{ clickable: true }] },
         { id: 'census-blocks', visible: false, layers: [{ clickable: true }] },
       ],
