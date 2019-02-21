9f93cba52bdd506161a988640ad122fceb9ba75c
allthesignals <wilburnforce@gmail.com>
2019-02-21 13:23:22 -0500

    Scope test to correct div

--- tests/acceptance/data-tables-sniff-test-test.js
+++ tests/acceptance/data-tables-sniff-test-test.js
@@ -9,61 +9,61 @@ module('Acceptance | data tables sniff test', function(hooks) {
   test('visiting assorted profiles, checking for content changes', async function(assert) {
     let text;
     await visit('/profile/1/demographic?reliability=true');
-    await percySnapshot('profile/1/demographic?reliability=true');
+    await percySnapshot('profile/1/demographic?reliability=true', { scope: '#profile-content' });
     assert.ok(true);
   });
 
   test('visiting /profile/1/social?reliability=true, checking for content changes', async function(assert) {
     await visit('/profile/1/social?reliability=true');
-    await percySnapshot('profile/1/social?reliability=true');
+    await percySnapshot('profile/1/social?reliability=true', { scope: '#profile-content' });
     assert.ok(true);
   });
 
   test('visiting /profile/1/economic?reliability=true, checking for content changes', async function(assert) {
     await visit('/profile/1/economic?reliability=true');
-    await percySnapshot('profile/1/economic?reliability=true');
+    await percySnapshot('profile/1/economic?reliability=true', { scope: '#profile-content' });
     assert.ok(true);
   });
 
   test('visiting /profile/1/housing?reliability=true, checking for content changes', async function(assert) {
     await visit('/profile/1/housing?reliability=true');
-    await percySnapshot('profile/1/housing?reliability=true');
+    await percySnapshot('profile/1/housing?reliability=true', { scope: '#profile-content' });
     assert.ok(true);
   });
 
   test('visiting /profile/1/census?reliability=true, checking for content changes', async function(assert) {
     await visit('/profile/1/census?reliability=true');
-    await percySnapshot('profile/1/census?reliability=true');
+    await percySnapshot('profile/1/census?reliability=true', { scope: '#profile-content' });
     assert.ok(true);
   });
 
   test('visiting /profile/726/demographic?reliability=true, checking for content changes', async function(assert) {
     await visit('/profile/726/demographic?reliability=true');
-    await percySnapshot('profile/726/demographic?reliability=true');
+    await percySnapshot('profile/726/demographic?reliability=true', { scope: '#profile-content' });
     assert.ok(true);
   });
 
   test('visiting /profile/726/social?reliability=true, checking for content changes', async function(assert) {
     await visit('/profile/726/social?reliability=true');
-    await percySnapshot('profile/726/social?reliability=true');
+    await percySnapshot('profile/726/social?reliability=true', { scope: '#profile-content' });
     assert.ok(true);
   });
 
   test('visiting /profile/726/economic?reliability=true, checking for content changes', async function(assert) {
     await visit('/profile/726/economic?reliability=true');
-    await percySnapshot('profile/726/economic?reliability=true');
+    await percySnapshot('profile/726/economic?reliability=true', { scope: '#profile-content' });
     assert.ok(true);
   });
 
   test('visiting /profile/726/housing?reliability=true, checking for content changes', async function(assert) {
     await visit('/profile/726/housing?reliability=true');
-    await percySnapshot('profile/726/housing?reliability=true');
+    await percySnapshot('profile/726/housing?reliability=true', { scope: '#profile-content' });
     assert.ok(true);
   });
 
   test('visiting /profile/726/census?reliability=true, checking for content changes', async function(assert) {
     await visit('/profile/726/census?reliability=true');
-    await percySnapshot('profile/726/census?reliability=true');
+    await percySnapshot('profile/726/census?reliability=true', { scope: '#profile-content' });
     assert.ok(true);
   });
 });
