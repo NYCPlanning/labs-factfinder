79c00dd4d598a030617de70734c26f4604101d42
trbmcginnis <taylormcgin@gmail.com>
2019-02-22 12:14:28 -0500

    603: add track-page mixin to router for analytics

--- app/router.js
+++ app/router.js
@@ -1,7 +1,8 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
+import trackPage from './mixins/track-page';
 
-const Router = EmberRouter.extend({
+const Router = EmberRouter.extend(trackPage, {
   location: config.locationType,
   rootURL: config.rootURL,
 });
