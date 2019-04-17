5e5894d0ff63e63e848ee259d7649e67bb2973a8
allthesignals <wilburnforce@gmail.com>
2019-04-17 11:48:04 -0400

    Add inline JSDoc to routes, models, mixins

--- app/routes/profile/index.js
+++ app/routes/profile/index.js
@@ -1,8 +1,17 @@
 import Route from '@ember/routing/route';
 
+/**
+ * The ProfileIndex Route is responsible for redirecting the user to a specific profile
+ * if they land on profile/index alone.
+ *
+ * See https://api.emberjs.com/ember/release/classes/Route
+ */
 export default Route.extend({
+  /**
+   * EmberJS Route hook called before the model hook is triggered.
+   * Here, it's used to direct the user from /profile to /profile/demographic
+   */
   beforeModel() {
-    // redirect /profile to /profile/demographic
     this.replaceWith('profile.demographic');
   },
 });
--- app/routes/profile/social.js
+++ app/routes/profile/social.js
@@ -1,10 +1,22 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
 import Downloadable from '../../mixins/downloadable';
 
+/**
+ * The Social Route is responsible for fetching the raw profile data for "social".
+ * NOTE: this route extends the Downloadable mixin, which reshapes the profile data
+ * that's returned from the server. See "mixins/downloadable.js"
+ *
+ * See https://api.emberjs.com/ember/release/classes/Route
+ */
 export default Route.extend(Downloadable, {
-  selection: service(),
-
+  /**
+   * EmberJS Route model hook, used here to fetch actual profile data from the
+   * API endpoint.
+   * @param {object} params - EmberJS object
+   * @param {object} transition - EmberJS object
+   * @param {object} transition.queryParams - EmberJS object
+   * @param {string} transition.queryParams.comparator - ID used for the profile's comparison geography
+   */
   model(params, { queryParams: { comparator = '0' } }) {
     const selectionId = this.modelFor('profile').id;
 
