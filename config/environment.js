2f6fba3492d0175abaed02e6caa811ab5df6dd47
Jonathan Pichot <jonathan@pichot.us>
2019-07-10 16:56:49 -0400

    point to production layers-api

--- config/environment.js
+++ config/environment.js
@@ -164,6 +164,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
+    ENV['mapbox-gl'].map.style = 'https://layers-api.planninglabs.nyc/v1/base/style.json';
+    ENV['ember-mapbox-composer'].host = 'https://layers-api.planninglabs.nyc';
     ENV.SupportServiceHost = 'https://factfinder-api.planninglabs.nyc';
   }
 
