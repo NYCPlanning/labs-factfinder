d22fe5788d06fe6fd5c26ed183dae3d41616d879
godfreyyeung <godfreyyeung@gmail.com>
2021-06-07 15:09:40 -0400

    3170 Set up Mirage passthroughs, Mapbox PBF monkeypatch

--- mirage/config.js
+++ mirage/config.js
@@ -1,4 +1,22 @@
+import patchXMLHTTPRequest from './helpers/mirage-mapbox-gl-monkeypatch';
+
 export default function() {
+  patchXMLHTTPRequest();
+
+  this.urlPrefix = 'https://factfinder-api.herokuapp.com';
+
+  this.passthrough('https://labs-layers-api.herokuapp.com/**');
+  this.passthrough('https://labs-layers-api-staging.herokuapp.com/v1/base/style.json');
+
+  this.passthrough('https://layers-api.planninglabs.nyc/**');
+  this.passthrough('https://layers-api-staging.planninglabs.nyc/**');
+  this.passthrough('https://search-api.planninglabs.nyc/**');
+  this.passthrough('https://tiles.planninglabs.nyc/**');
+
+  this.passthrough('https://planninglabs.carto.com/**');
+
+  this.passthrough('https://raw.githubusercontent.com/**');
+  this.passthrough('http://raw.githubusercontent.com/**');
 
   /*
     Config (with defaults).
@@ -6,7 +24,6 @@ export default function() {
     Note: these only affect routes defined *after* them!
   */
 
-  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
   // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
   // this.timing = 400;      // delay for each request, automatically set to 0 during testing
 
