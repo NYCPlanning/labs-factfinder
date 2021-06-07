782e4b7da8b61d3695ebb1e57e1aff749aac6527
godfreyyeung <godfreyyeung@gmail.com>
2021-06-07 15:23:09 -0400

    3170 explorer route, Mirage mock endpt and default data

--- app/routes/explorer.js
+++ app/routes/explorer.js
@@ -1,5 +1,17 @@
 import Route from '@ember/routing/route';
+import Environment from '../config/environment';
 
-export default Route.extend({
+const { SupportServiceHost } = Environment;
 
-});
+const SELECTION_API_URL = id => `${SupportServiceHost}/explorer/${id}`;
+
+export default class ExplorerRoute extends Route {
+  model({ id }) { // eslint-disable-line
+    if (id) {
+      return fetch(SELECTION_API_URL(id))
+        .then(response => response.json());
+    }
+
+    return fetch(SELECTION_API_URL('nyc'));
+  }
+}
--- mirage/config.js
+++ mirage/config.js
@@ -1,10 +1,38 @@
+import profilesAllNTA from './static/profile-all-nta';
+import selectionAllNTA from './static/selection-all-nta';
 import patchXMLHTTPRequest from './helpers/mirage-mapbox-gl-monkeypatch';
 
 export default function() {
   patchXMLHTTPRequest();
 
+  /*
+    Config
+    Note: these only affect routes defined *after* them!
+
+    this.namespace = '';    // make this `/api`, for example, if your API is namespaced
+    this.timing = 400;      // delay for each request, automatically set to 0 during testing
+  */
   this.urlPrefix = 'https://factfinder-api.herokuapp.com';
 
+  /*
+    Routes
+
+    Shorthand cheatsheet:
+    this.get('/posts');
+    this.post('/posts');
+    this.get('/posts/:id');
+    this.put('/posts/:id'); // or this.patch
+    this.del('/posts/:id');
+  */
+  this.get('/explorer/:id', () => ({
+    profile: profilesAllNTA,
+    selection: selectionAllNTA,
+  }));
+
+
+  /*
+    Passthroughs
+  */
   this.passthrough('https://labs-layers-api.herokuapp.com/**');
   this.passthrough('https://labs-layers-api-staging.herokuapp.com/v1/base/style.json');
 
@@ -17,25 +45,4 @@ export default function() {
 
   this.passthrough('https://raw.githubusercontent.com/**');
   this.passthrough('http://raw.githubusercontent.com/**');
-
-  /*
-    Config (with defaults).
-
-    Note: these only affect routes defined *after* them!
-  */
-
-  // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
-  // this.timing = 400;      // delay for each request, automatically set to 0 during testing
-
-  /*
-    Shorthand cheatsheet:
-
-    this.get('/posts');
-    this.post('/posts');
-    this.get('/posts/:id');
-    this.put('/posts/:id'); // or this.patch
-    this.del('/posts/:id');
-
-    https://www.ember-cli-mirage.com/docs/route-handlers/shorthands
-  */
 }
