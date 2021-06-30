1b1f371977e838c3e624c2e580e3325a8432c1b1
godfreyyeung <godfreyyeung@gmail.com>
2021-06-30 12:44:09 -0400

    3196 Add basic example charts for some ACS

---
+++ app/chart-config/demographic/index.js
@@ -0,0 +1,9 @@
+import raceGroupChartConfig from './race-group';
+import hispanicSubgroupChartConfig from './hispanic-subgroup';
+import asianSubgroupChartConfig from './asian-subgroup';
+
+export default {
+  raceGroupChartConfig,
+  hispanicSubgroupChartConfig,
+  asianSubgroupChartConfig,
+};
