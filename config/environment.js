f1639eeef5c6ef8eebbbbfbfef2ff6e2bbbcafc9
dhochbaum-dcp <dhochbaum@planning.nyc.gov>
2021-07-06 13:08:23 -0400

    Moved icons to their own config file, as configuring in env.js will be removed in the future.

--- config/environment.js
+++ config/environment.js
@@ -69,50 +69,6 @@ module.exports = function (environment) {
       },
     },
 
-    fontawesome: {
-      icons: {
-        'free-solid-svg-icons': [
-          'arrow-left',
-          'arrow-right',
-          'arrow-up',
-          'arrow-down',
-          'building',
-          'caret-down',
-          'caret-up',
-          'download',
-          'edit',
-          'exclamation-triangle',
-          'external-link-alt',
-          'file',
-          'info-circle',
-          'long-arrow-alt-right',
-          'map',
-          'map-pin',
-          'pen',
-          'plus',
-          'question-circle',
-          'search',
-          'sliders-h',
-          'square',
-          'times',
-          'user-friends',
-        ],
-        'free-regular-svg-icons': [
-          'arrow-alt-circle-down',
-          'check-square',
-          'circle',
-          'copy',
-          'dot-circle',
-          'envelope',
-          'hand-pointer',
-          'object-group',
-          'square',
-          'check-square',
-          'minus-square'
-        ],
-      },
-    },
-
     metricsAdapters: [
       {
         name: 'GoogleAnalytics',
