f531f1e4fa0302e62383c4811d319fe4f457dc97
godfreyyeung <godfreyyeung@gmail.com>
2021-09-13 17:46:21 -0400

    4847 Update Decennial table configs

--- app/table-config/census/housing-occupancy.js
+++ app/table-config/census/housing-occupancy.js
@@ -1,57 +1,14 @@
 export default [
   {
-    highlight: true,
-    title: 'Total housing units',
     data: 'hunits',
+    title: 'Total housing units',
   },
   {
-    title: 'Occupied housing units',
     data: 'ochu_1',
+    title: 'Occupied housing units',
   },
   {
-    title: 'Vacant housing units',
     data: 'vachus',
-  },
-  {
-    indent: 1,
-    title: 'For rent',
-    data: 'vhufrnt',
-  },
-  {
-    indent: 1,
-    title: 'For sale only',
-    data: 'vhufslo',
-  },
-  {
-    indent: 1,
-    title: 'Rented or sold, not occupied',
-    data: 'vhurosnoc',
-  },
-  {
-    indent: 1,
-    title: 'For seasonal, recreational, or occasional use',
-    data: 'vhufsroou',
-  },
-  {
-    indent: 1,
-    title: 'Other vacant',
-    data: 'vhuothvc',
-  },
-  {
-    divider: true,
-  },
-  {
-    title: 'Homeowner vacancy rate (percent)',
-    tooltip: 'Number of vacant units "for sale only," divided by sum of owner-occupied units and vacant units "for sale only," multiplied by 100. (Used definition from 2000 for consistency in measuring change.)',
-    data: 'hmownrvcrt',
-    decimal: 1,
-    special: true,
-  },
-  {
-    title: 'Rental vacancy rate (percent)',
-    tooltip: 'Number of vacant units "for rent," divided by sum of renter-occupied units and vacant units "for rent," multiplied by 100. (Used definition from 2000 for consistency in measuring change.)',
-    data: 'rntvcrt',
-    decimal: 1,
-    special: true,
-  },
+    title: 'Vacant housing units',
+  }
 ];
