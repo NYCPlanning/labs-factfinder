00fb9ad0d096774778942a55f09bc3ef88919395
dhochbaum-dcp <dhochbaum@planning.nyc.gov>
2021-09-29 17:52:13 -0400

    Updated env file due to api pr 150

--- config/environment.js
+++ config/environment.js
@@ -101,7 +101,7 @@ module.exports = function (environment) {
   };
 
 if (environment === 'local-api') {
-  ENV.SupportServiceHost = 'http://localhost:3000';
+  ENV.SupportServiceHost = 'http://localhost:3001';
   ENV['mapbox-gl'].map.style = 'http://localhost:3120/v1/base/style.json';
   ENV['ember-mapbox-composer'].host = 'http://localhost:3120';
   ENV['ember-cli-mirage'] = {
@@ -129,7 +129,7 @@ if (environment === 'local-api') {
   }
 
   if (environment === 'devlocal') {
-    ENV.SupportServiceHost = 'http://localhost:3000';
+    ENV.SupportServiceHost = 'http://localhost:3001';
   }
 
   if (environment === 'test') {
