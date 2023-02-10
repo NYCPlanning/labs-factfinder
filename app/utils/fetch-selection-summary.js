c81e93dd909eb9e0a172ed21befed4cf1b0b96ee
horatio <horatiorosa@aol.com>
2023-02-10 12:01:33 -0500

    11704 make changes to how selection requests are sent to the labs-factfinder-api Usually only census tracts or census blocks requests have the potential to exceed the API GET limits so this c so this change is targeting those specific requests. Selections;  - from 500 to 6999 as a single POST request  - 7000 or greater, sent in incrememts of 7000  - 500 or less, sent as a GET request

--- app/utils/fetch-selection-summary.js
+++ app/utils/fetch-selection-summary.js
@@ -3,14 +3,50 @@ import Environment from '../config/environment';
 const { SupportServiceHost } = Environment;
 
 const SELECTION_SUMMARY_API_URL = (survey = 'decennial', summaryVars = 'pop1,hunits', geoid = '0') => `${SupportServiceHost}/summary/${survey}/${summaryVars}/${geoid}`;
-
+const SELECTION_SUMMARY_API_URL_POST = (survey = 'decennial', summaryVars = 'pop1,hunits') => `${SupportServiceHost}/summary/${survey}/${summaryVars}`;
 
 export default async function fetchSelectionSummary(survey, summaryVars, geoid) {
-  if(geoid.length) {
+  // to ensure we don't receive errors from the URLs being too long, we do multiple queries of 500 items each
+  if (geoid.length > 7000) {
+    let selectionResponse = {totals: { hunits: 0, pop1: 0 }};
+    let partialSelectionResponse = null;
+
+    for (let i=0; i<geoid.length; i+=7000) {
+      partialSelectionResponse = await fetch(SELECTION_SUMMARY_API_URL_POST(survey, summaryVars), {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+
+        },
+        body: JSON.stringify(geoid.slice(i, i+6999)),
+      });
+
+      partialSelectionResponse = await partialSelectionResponse.json();
+      selectionResponse.totals.hunits += partialSelectionResponse.totals.hunits;
+      selectionResponse.totals.pop1 += partialSelectionResponse.totals.pop1;
+    }
+
+    return selectionResponse;
+
+  } else if (geoid.length > 500) {
+    const response = await fetch(SELECTION_SUMMARY_API_URL_POST(survey, summaryVars), {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+
+      },
+      body: JSON.stringify(geoid),
+    });
+
+    return response.json();
+
+  } else if (geoid.length) {
     let selectionResponse = null;
     selectionResponse = await fetch(SELECTION_SUMMARY_API_URL(survey, summaryVars, geoid));
     selectionResponse = await selectionResponse.json();
+
     return selectionResponse;
   }
+
   return null;
 }
