ddd3e120d1dfc699f7b350e79e5fa6152548b6a5
horatio <horatiorosa@aol.com>
2023-02-24 15:26:33 -0500

    11704 refactored fetch selectuin-summary.js to only allow a max selection of 1000 geoids. Added messaging to map utility to inform uses if they have exceeded max geoselections

--- app/utils/fetch-selection-summary.js
+++ app/utils/fetch-selection-summary.js
@@ -6,46 +6,30 @@ const SELECTION_SUMMARY_API_URL = (survey = 'decennial', summaryVars = 'pop1,hun
 const SELECTION_SUMMARY_API_URL_POST = (survey = 'decennial', summaryVars = 'pop1,hunits') => `${SupportServiceHost}/summary/${survey}/${summaryVars}`;
 
 export default async function fetchSelectionSummary(survey, summaryVars, geoid) {
-  // to ensure we don't receive errors from the URLs being too long, we do multiple queries of 500 items each
-  if (geoid.length > 7000) {
-    let selectionResponse = {totals: { hunits: 0, pop1: 0 }};
-    let partialSelectionResponse = null;
-
-    for (let i=0; i<geoid.length; i+=7000) {
-      partialSelectionResponse = await fetch(SELECTION_SUMMARY_API_URL_POST(survey, summaryVars), {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-
-        },
-        body: JSON.stringify(geoid.slice(i, i+6999)),
-      });
-
-      partialSelectionResponse = await partialSelectionResponse.json();
-      selectionResponse.totals.hunits += partialSelectionResponse.totals.hunits;
-      selectionResponse.totals.pop1 += partialSelectionResponse.totals.pop1;
-    }
+  let selectionResponse = {totals: { hunits: 0, pop1: 0 }};
+  // to ensure we don't receive errors from the URLs being too long, we limit the selections to 1500 geoids
+  if (geoid.length > 1000) {
+    selectionResponse = null;
 
     return selectionResponse;
 
-  } else if (geoid.length > 500) {
+  } else if (geoid.length > 100) {
     const response = await fetch(SELECTION_SUMMARY_API_URL_POST(survey, summaryVars), {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-
       },
       body: JSON.stringify(geoid),
     });
 
-    return response.json();
-
-  } else if (geoid.length) {
-    let selectionResponse = null;
-    selectionResponse = await fetch(SELECTION_SUMMARY_API_URL(survey, summaryVars, geoid));
-    selectionResponse = await selectionResponse.json();
+    selectionResponse = response.json();
 
     return selectionResponse;
+  } else if (geoid.length) {
+      selectionResponse = await fetch(SELECTION_SUMMARY_API_URL(survey, summaryVars, geoid));
+      selectionResponse = await selectionResponse.json();
+
+      return selectionResponse;
   }
 
   return null;
