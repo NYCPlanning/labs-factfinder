1d2848a20bcf42412a9bd149ffd299cbd8c1ef89
horatio <horatiorosa@aol.com>
2023-03-06 15:30:42 -0500

    ready for code review, limit set at 500 geoids

--- app/utils/fetch-selection-summary.js
+++ app/utils/fetch-selection-summary.js
@@ -2,19 +2,19 @@ import Environment from '../config/environment';
 
 const { SupportServiceHost } = Environment;
 
-const SELECTION_SUMMARY_API_URL = (survey = 'decennial', summaryVars = 'pop1,hunits', geoid = '0') => `${SupportServiceHost}/summary/${survey}/${summaryVars}/${geoid}`;
-const SELECTION_SUMMARY_API_URL_POST = (survey = 'decennial', summaryVars = 'pop1,hunits') => `${SupportServiceHost}/summary/${survey}/${summaryVars}`;
+const SELECTION_SUMMARY_API_URL = (survey = 'decennial', summaryVars = 'pop1,hunits') => `${SupportServiceHost}/summary/${survey}/${summaryVars}`;
 
 export default async function fetchSelectionSummary(survey, summaryVars, geoid) {
   let selectionResponse = {totals: { hunits: 0, pop1: 0 }};
-  // to ensure we don't receive errors from the URLs being too long, we limit the selections to 1500 geoids
-  if (geoid.length > 5000) {
+  // to ensure we don't receive errors from the URLs being too long, we limit the selections to 500 geoids
+  // The limit can be increased when indices are added to the geoids in the pff db
+  if (geoid.length > 500) {
     selectionResponse = null;
 
     return selectionResponse;
 
-  } else if (geoid.length > 100) {
-    const response = await fetch(SELECTION_SUMMARY_API_URL_POST(survey, summaryVars), {
+  } else if (geoid.length) {
+    const response = await fetch(SELECTION_SUMMARY_API_URL(survey, summaryVars), {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -25,11 +25,6 @@ export default async function fetchSelectionSummary(survey, summaryVars, geoid) 
     selectionResponse = response.json();
 
     return selectionResponse;
-  } else if (geoid.length) {
-      selectionResponse = await fetch(SELECTION_SUMMARY_API_URL(survey, summaryVars, geoid));
-      selectionResponse = await selectionResponse.json();
-
-      return selectionResponse;
   }
 
   return null;
