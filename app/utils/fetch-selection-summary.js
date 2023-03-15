d6daf8db35fed470858af8bdf0f214c9c10461ab
horatio <horatiorosa@aol.com>
2023-03-15 13:07:31 -0400

    11704 code review updates & changes

--- app/utils/fetch-selection-summary.js
+++ app/utils/fetch-selection-summary.js
@@ -4,28 +4,33 @@ const { SupportServiceHost } = Environment;
 
 const SELECTION_SUMMARY_API_URL = (survey = 'decennial', summaryVars = 'pop1,hunits') => `${SupportServiceHost}/summary/${survey}/${summaryVars}`;
 
+const maxGeoidLength = 500;
+
 export default async function fetchSelectionSummary(survey, summaryVars, geoid) {
-  let selectionResponse = {totals: { hunits: 0, pop1: 0 }};
-  // to ensure we don't receive errors from the URLs being too long, we limit the selections to 500 geoids
-  // The limit can be increased when indices are added to the geoids in the pff db
-  if (geoid.length > 500) {
-    selectionResponse = null;
+  try {
+    let selectionResponse = {totals: { hunits: 0, pop1: 0 }};
+    // to ensure we don't receive errors from the URLs being too long, we limit the selections to 500 geoids
+    // The limit can be increased when indices are added to the geoids in the pff db
+    if (geoid.length > maxGeoidLength) {
+      throw RangeError(`geoid selection of ${geoid.length} is greater than than permitted maximum of ${maxGeoidLength}`)
 
-    return selectionResponse;
+    } else if (geoid.length) {
+      const response = await fetch(SELECTION_SUMMARY_API_URL(survey, summaryVars), {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(geoid),
+      });
 
-  } else if (geoid.length) {
-    const response = await fetch(SELECTION_SUMMARY_API_URL(survey, summaryVars), {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(geoid),
-    });
+      selectionResponse = response.json();
 
-    selectionResponse = response.json();
+      return selectionResponse;
+    }
 
-    return selectionResponse;
+    return null;
+
+  } catch (e) {
+    throw Error(`Error in fetching selection summary`, e, e.message);
   }
-
-  return null;
 }
