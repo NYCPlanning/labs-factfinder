e3ac36d33de497aba4cab052eb7e70f34b8cb84e
horatio <horatiorosa@aol.com>
2024-02-26 09:35:57 -0500

    Issue 1133 added sub header text to DT&E header

--- app/topics-config/census.js
+++ app/topics-config/census.js
@@ -29,6 +29,8 @@ export default [
   {
     id: 'detailedRaceAndEthnicity',
     label: 'Detailed Race and Ethnicity',
+    labelText:
+      'Detailed race and ethnicity data represent the group alone or in any combination. The Census Bureau only publishes detailed race and ethnicity data for NYC at a tract, county, and city level. All other geographies are calculated by the Department of City Planning. Because of suppression, aggregate counts incorporate nonsampling error.',
     selected: 'unselected',
     type: 'subtopic',
     tableConfig: censusTableConfig.detailedRaceAndEthnicity,
